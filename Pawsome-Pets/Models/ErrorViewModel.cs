8c8da4d5e8aadbd8540add80c37c833fd2589e76
Ivan-Spawnz-Zhelyazkov <i.zhelyazkov96@gmail.com>
2025-07-16 13:54:47 +0300

    init: inital commit

---
+++ Pawsome-Pets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pawsome_Pets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
